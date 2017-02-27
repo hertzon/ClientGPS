3249a3084ef5cc321fc51b7a8c965ae94fbe6af0
nelson rodriguez <nelsonhenge@hotmail.com>
2017-02-26 21:57:11 -0500

    26/02/2017

--- app/src/main/java/com/ejemplos/nelson/clientgps/MainActivity.java
+++ app/src/main/java/com/ejemplos/nelson/clientgps/MainActivity.java
@@ -13,6 +13,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.SystemClock;
 //import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
@@ -28,19 +29,11 @@ import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.UUID;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
     private static final String TAG = "GPSTrax";
-
-    // use the websmithing defaultUploadWebsite for testing and then check your
-    // location with your browser here: https://www.websmithing.com/gpstracker/displaymap.php
     private String defaultUploadWebsite;
-
-    private EditText txtUserName;
-    private EditText txtWebsite;
     private Button trackingButton;
-
     private boolean currentlyTracking;
-    private RadioGroup intervalRadioGroup;
     private int intervalInMinutes = 1;
     private AlarmManager alarmManager;
     private Intent gpsTrackerIntent;
@@ -59,15 +52,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         contextOfApplication = getApplicationContext();
-        //getSupportActionBar().setDisplayShowHomeEnabled(true);
-        //getSupportActionBar().setLogo(R.mipmap.ic_launcher);
-        //getSupportActionBar().setDisplayUseLogoEnabled(true);
-        TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        imei = tm.getDeviceId();
+
+
+
         Log.d(TAG,"Starting Aplication GPSTrax.....");
         Log.d(TAG,"Created by Nelson Rodriguez 01/02/2017");
-        Log.d(TAG,"IMEI equipo: "+imei);
-
         Log.d(TAG,"Revisando si esta habilitado el posicionamiento...");
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
@@ -78,17 +67,11 @@ public class MainActivity extends Activity {
             Toast.makeText(getApplicationContext(),"Debe habilitar el GPS!!!",Toast.LENGTH_SHORT).show();
             showGPSDisabledAlertToUser();
         }
-
         defaultUploadWebsite = getString(R.string.default_upload_website);
-
-        txtWebsite = (EditText)findViewById(R.id.txtWebsite);
-        txtUserName = (EditText)findViewById(R.id.txtUserName);
-        intervalRadioGroup = (RadioGroup)findViewById(R.id.intervalRadioGroup);
         trackingButton = (Button)findViewById(R.id.trackingButton);
-        txtUserName.setImeOptions(EditorInfo.IME_ACTION_DONE);
-
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
         currentlyTracking = sharedPreferences.getBoolean("currentlyTracking", false);
+        Log.d(TAG,"currentlyTracking(1): "+currentlyTracking);
         registroHecho=sharedPreferences.getBoolean("registroHecho", false);
         Log.d(TAG,"registroHecho inicio: "+registroHecho);
 
@@ -109,31 +92,28 @@ public class MainActivity extends Activity {
             Log.d(TAG,"strUsuario: "+strUsuario);
             Log.d(TAG,"strClave: "+strClave);
             Log.d(TAG,"imei: "+imei);
-
-
         }
 
 
 
-        Log.d(TAG,"currentlyTracking 1: "+currentlyTracking);
 
+
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Log.d(TAG,"Guardando alarmState false");
+        editor.putBoolean("alarmState",false);
+        Log.d(TAG,"Guardando intervalo fijo de 1 minuto...");
+        editor.putInt("intervalInMinutes", 1);
+        editor.apply();
+        Log.d(TAG,"currentlyTracking 1: "+currentlyTracking);
         boolean firstTimeLoadingApp = sharedPreferences.getBoolean("firstTimeLoadingApp", true);
         Log.d(TAG,"firstTimeLoadingApp 1: "+firstTimeLoadingApp);
         if (firstTimeLoadingApp) {
-            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor = sharedPreferences.edit();
             editor.putBoolean("firstTimeLoadingApp", false);
             editor.putString("appID",  UUID.randomUUID().toString());
             editor.apply();
         }
-
-
-        intervalRadioGroup.setOnCheckedChangeListener(
-                new RadioGroup.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                        saveInterval();
-                    }
-                });
 
         trackingButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -164,31 +144,7 @@ public class MainActivity extends Activity {
         AlertDialog alert = alertDialogBuilder.create();
         alert.show();
     }
-
-    private void saveInterval() {
-        if (currentlyTracking) {
-            Toast.makeText(getApplicationContext(), R.string.user_needs_to_restart_tracking, Toast.LENGTH_LONG).show();
-        }
-
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        switch (intervalRadioGroup.getCheckedRadioButtonId()) {
-            case R.id.i1:
-                editor.putInt("intervalInMinutes", 1);
-                break;
-            case R.id.i5:
-                editor.putInt("intervalInMinutes", 5);
-                break;
-            case R.id.i15:
-                editor.putInt("intervalInMinutes", 15);
-                break;
-        }
-
-        editor.apply();
-    }
-
-    private void startAlarmManager() {
+    public void startAlarmManager() {
         Log.d(TAG, "startAlarmManager");
 
         Context context = getBaseContext();
@@ -219,22 +175,18 @@ public class MainActivity extends Activity {
     protected void trackLocation(View v) {
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        if (!saveUserSettings()) {
-            return;
-        }
-
         if (!checkIfGooglePlayEnabled()) {
             return;
         }
-
         if (currentlyTracking) {
+            Log.d(TAG,"currentlyTracking:"+currentlyTracking);
             cancelAlarmManager();
 
             currentlyTracking = false;
             editor.putBoolean("currentlyTracking", false);
             editor.putString("sessionID", "");
         } else {
+            Log.d(TAG,"currentlyTracking:"+currentlyTracking);
             startAlarmManager();
 
             currentlyTracking = true;
@@ -243,94 +195,31 @@ public class MainActivity extends Activity {
             editor.putBoolean("firstTimeGettingPosition", true);
             editor.putString("sessionID",  UUID.randomUUID().toString());
         }
-
         editor.apply();
         setTrackingButtonState();
     }
-
-    private boolean saveUserSettings() {
-        if (textFieldsAreEmptyOrHaveSpaces()) {
-            return false;
-        }
-
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        switch (intervalRadioGroup.getCheckedRadioButtonId()) {
-            case R.id.i1:
-                editor.putInt("intervalInMinutes", 1);
-                break;
-            case R.id.i5:
-                editor.putInt("intervalInMinutes", 5);
-                break;
-            case R.id.i15:
-                editor.putInt("intervalInMinutes", 15);
-                break;
-        }
-
-        editor.putString("userName", txtUserName.getText().toString().trim());
-        editor.putString("defaultUploadWebsite", txtWebsite.getText().toString().trim());
-
-        editor.apply();
-
-        return true;
-    }
-
-    private boolean textFieldsAreEmptyOrHaveSpaces() {
-        String tempUserName = txtUserName.getText().toString().trim();
-        String tempWebsite = txtWebsite.getText().toString().trim();
-
-        if (tempWebsite.length() == 0 || hasSpaces(tempWebsite) || tempUserName.length() == 0 || hasSpaces(tempUserName)) {
-            Toast.makeText(this, R.string.textfields_empty_or_spaces, Toast.LENGTH_LONG).show();
-            return true;
-        }
-
-        return false;
-    }
-
     private boolean hasSpaces(String str) {
         return ((str.split(" ").length > 1) ? true : false);
     }
-
-    private void displayUserSettings() {
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
-        intervalInMinutes = sharedPreferences.getInt("intervalInMinutes", 1);
-
-        switch (intervalInMinutes) {
-            case 1:
-                intervalRadioGroup.check(R.id.i1);
-                break;
-            case 5:
-                intervalRadioGroup.check(R.id.i5);
-                break;
-            case 15:
-                intervalRadioGroup.check(R.id.i15);
-                break;
-        }
-
-        txtWebsite.setText(sharedPreferences.getString("defaultUploadWebsite", defaultUploadWebsite));
-        txtUserName.setText(sharedPreferences.getString("userName", ""));
-    }
-
     private boolean checkIfGooglePlayEnabled() {
         if (GooglePlayServicesUtil.isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS) {
             return true;
         } else {
             Log.e(TAG, "unable to connect to google play services.");
-            Toast.makeText(getApplicationContext(), R.string.google_play_services_unavailable, Toast.LENGTH_LONG).show();
+            //Toast.makeText(getApplicationContext(), R.string.google_play_services_unavailable, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Por favor habilite Google Play Services!!", Toast.LENGTH_LONG).show();
             return false;
         }
     }
-
     private void setTrackingButtonState() {
         if (currentlyTracking) {
             trackingButton.setBackgroundResource(R.drawable.green_tracking_button);
             trackingButton.setTextColor(Color.BLACK);
-            trackingButton.setText(R.string.tracking_is_on);
+            trackingButton.setText("Rastreando equipo movil!");
         } else {
             trackingButton.setBackgroundResource(R.drawable.red_tracking_button);
             trackingButton.setTextColor(Color.WHITE);
-            trackingButton.setText(R.string.tracking_is_off);
+            trackingButton.setText("Rastreo Apagado!");
         }
     }
     public String getImei(){
@@ -354,13 +243,25 @@ public class MainActivity extends Activity {
         Log.d(TAG,"imei: "+imei);
         registroHecho=sharedPreferences.getBoolean("registroHecho", false);
 
-        if (registroHecho){
+        boolean estadoAlarma=sharedPreferences.getBoolean("alarmState",false);
+        Log.d(TAG,"alarmState: "+estadoAlarma);
+        if (!estadoAlarma && registroHecho){
+            Log.d(TAG,"Arrancando alarmanager(1)...");
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            startAlarmManager();
+            editor.putBoolean("alarmState",true);
+            editor.apply();
+            currentlyTracking=true;
+        }
+        if (registroHecho && currentlyTracking){
             Log.d(TAG,"Arrancando intent GpsTrackerAlarmReceiver");
             this.startService(new Intent(this, LocationService.class));
         }
 
 
-        displayUserSettings();
+
+
+        //displayUserSettings();
         setTrackingButtonState();
     }
     public static Context getContextOfApplication(){
@@ -370,6 +271,18 @@ public class MainActivity extends Activity {
     @Override
     protected void onStop() {
         Log.d(TAG,"onStop MainActivity");
+
         super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG,"onDestroy MainActivity");
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("alarmState",false);
+        editor.apply();
+        Log.d(TAG,"chao!!!!");
+        super.onDestroy();
     }
 }
